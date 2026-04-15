648ae0a385d33a96190fd4625c02d7b0ac74f77b
potlapallichandrika <chandrikaip4@gmail.com>
2026-04-15 17:18:51 +0530

    version changed

--- mod/customactivity/version.php
+++ mod/customactivity/version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
+
 $plugin->component = 'mod_customactivity';
-$plugin->version = 2025121101;
-$plugin->requires = 2022041900; // Moodle 3.5!
-$plugin->maturity = MATURITY_STABLE;
-//$plugin->release = '4.2.2';
+$plugin->version   = 2025121102;
+$plugin->requires  = 2022041900;
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '5.0 (AI Smart Grading with o3-mini + Feedback)';
