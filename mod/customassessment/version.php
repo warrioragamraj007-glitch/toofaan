1a0bdd6fdae9670edf5a6ada474a804159edffc6
potlapallichandrika <chandrikaip4@gmail.com>
2026-04-17 13:15:42 +0530

    version

--- mod/customassessment/version.php
+++ mod/customassessment/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_customassessment';
-$plugin->version   = 2026041601; // 🔥 updated
+$plugin->version   = 2026011204;
 $plugin->requires  = 2022041900; // Moodle 4.x
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.1';
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '1.0';
